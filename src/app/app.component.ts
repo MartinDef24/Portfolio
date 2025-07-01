4a25dfeee8de318a575d278198646ff2d101f832
MartinDefachelles <martin@battlekart.com>
2025-07-01 19:38:18 +0200

    header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 @Component({
     selector: 'app-root',
-    standalone:true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    imports: [Header, Footer],
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
 }
---
+++ src/app/components/header/header.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone:true,
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
