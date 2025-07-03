4d48c8cc47941fed11cf0935d2ae062098559c4b
MartinDefachelles <martin@battlekart.com>
2025-07-03 13:14:29 +0200

    homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [Header, Footer],
+    standalone:true,
+    imports: [Header, Footer, RouterOutlet],
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
