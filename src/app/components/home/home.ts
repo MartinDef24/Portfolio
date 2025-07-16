4b2737d3c924d7b29e557d53026036430addcbe4
martindefachelles <martin@battlekart.com>
2025-07-16 13:22:45 +0200

    fix slider

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -14,41 +14,28 @@ import { ProjectInterface } from "../../interfaces/project-interface";
 export class Home implements OnInit {
   projects: ProjectInterface[] = [];
   currentIndex: number = 0;
+  cardWidth: number = 30;       // largeur fixe de chaque carte
+  cardGapPx: number = 3;        // espace entre les cartes
+  visibleItems: number = 3;
 
-  constructor(private projectService: ProjectsService) {
-  }
+  constructor(private projectService: ProjectsService) {}
 
   ngOnInit() {
     this.projects = this.projectService.project();
   }
-  get displayedProjects(): ProjectInterface[] {
-    const result: ProjectInterface[] = [];
-    const numProjects = this.projects.length;
-
-    if (numProjects === 0) {
-      return [];
-    }
-    if (numProjects <= 3) {
-      return [...this.projects];
-    }
-
-    for (let i = 0; i < 3; i++) {
-      const projectIndex = (this.currentIndex + i) % numProjects;
-      result.push(this.projects[projectIndex]);
-    }
-    return result;
-  }
 
   nextProject(): void {
-    if (this.projects.length === 0) {
-      return;
-    }
+    if (this.projects.length <= this.visibleItems) return;
     this.currentIndex = (this.currentIndex + 1) % this.projects.length;
-  }
-  prevProject(): void {
-    if (this.projects.length === 0) {
-      return;
+
+    if (this.currentIndex + this.visibleItems > this.projects.length) {
+      this.currentIndex = 0;
     }
-    this.currentIndex = (this.currentIndex - 1 + this.projects.length) % this.projects.length;
+  }
+
+  prevProject(): void {
+    if (this.projects.length <= this.visibleItems) return;
+    this.currentIndex =
+      (this.currentIndex - 1 + this.projects.length) % this.projects.length;
   }
 }
