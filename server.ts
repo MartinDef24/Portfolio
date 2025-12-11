5f7a8d90b89ebcdc2b6c98cfdd5b51d69f45c433
martindefachelles <martin@battlekart.com>
2025-12-11 20:22:20 +0100

    server.ts coolify

--- server.ts
+++ server.ts
@@ -1,24 +1,26 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 
 const server = express();
 
-// Chemin vers le dossier dist/portfolio
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const distFolder = resolve(__dirname, 'dist/portfolio');
 
-// Servir les fichiers statiques
 server.use(express.static(distFolder));
 
-// Toutes les autres routes renvoient index.html
-server.get('*', (req, res) => {
+server.get('*', (req: Request, res: Response) => {
   res.sendFile(join(distFolder, 'index.html'));
 });
 
-// Démarrer le serveur
-const port = Number(process.env['PORT']) || 4000;
-server.listen(port, () => {
-  console.log(`Serveur démarré sur http://localhost:${port}`);
+
+const port = Number(process.env['PORT']) || 3000;
+
+const host = '0.0.0.0'; 
+
+server.listen(port, host, () => {
+  console.log(`Serveur Express démarré sur http://${host}:${port}`);
+  console.log(`Serveur servant les fichiers depuis: ${distFolder}`);
 });
